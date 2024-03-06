0afc77bd35766e8a77d149fc4c1c7493cfd43fe8
luke <xiaopikaqiu@outlook.com>
2024-03-06 22:55:26 +0800

    Unmake the status bar translucent

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wx_reader_book/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 
+  //Unmake the status bar translucent
+  SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark
+  );
+  SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
 
 }
 

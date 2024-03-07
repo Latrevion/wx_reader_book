40343bfaf0c56a5160951dd55b36e71babaf1c5e
luzhihong <xiaopikaqiu@outlook.com>
2024-03-07 09:43:41 +0800

    finish the search_bar.dart

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -8,7 +8,7 @@ class DiscoverPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CustomSearchBar()
+        CustomSearchBar(rightButton:Text('Discover'))
       ],
     );
   }
--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomSearchBar extends StatelessWidget{
-  const CustomSearchBar({super.key});
+  final Widget rightButton;
+
+  const CustomSearchBar({super.key,required this.rightButton});
 
   @override
   Widget build(BuildContext context) {
    return Container(
      width: double.infinity,
-     height: 92,
-     color: Colors.blue,
-     padding: const EdgeInsets.only(left: 20,right: 20,top: 35,bottom: 16),
+     height: 61,
+     padding: const EdgeInsets.only(left: 13,right: 13,top: 24,bottom: 10),
      child: Container(
-       height: 39,
-       color: const Color(0xfff0f1f4),
+       height: 26,
+       decoration: BoxDecoration(
+         borderRadius: BorderRadius.circular(20),
+         color: const Color(0xfff0f1f4),
+       ),
+       child: Row(
+         children: [
+           const SizedBox(width: 13,),
+           const Icon(Icons.search,size: 13,color: Colors.black45,),
+           const SizedBox(width: 5),
+           const Text('undercurrent',style: TextStyle(fontSize: 11,color:Colors.black45,)),
+           const Expanded(child: SizedBox(height: 1,)),
+           Container(width: 0.5,height: 11,color: const Color(0xffb5b5b8),),
+           const SizedBox(width: 11,),
+           rightButton,
+           const SizedBox(width: 13,)
+         ],
+       ),
      ),
    );
   }
